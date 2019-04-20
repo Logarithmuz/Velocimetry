4c60b9938b1858a0e0b672ecb15916cd6f8ef6c7
Logarithmuz <40320189+Logarithmuz@users.noreply.github.com>
2019-04-20 12:08:19 +0200

    Added check if entry already is in DB

--- src/main/java/de/velocimetry/DataImporter.java
+++ src/main/java/de/velocimetry/DataImporter.java
@@ -4,12 +4,13 @@ import java.sql.SQLException;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 public class DataImporter {
 
 	private SQLConnection sqlConnection;
-	private List<SpeedMeasurement> speedMeasurementList = new ArrayList<SpeedMeasurement>();
+	private Map<Integer, List<SpeedMeasurement>> speedMeasurements;
 
 	public DataImporter(SQLConnection sqlConnection) {
 		super();
@@ -22,6 +23,7 @@ public class DataImporter {
 	public boolean importFromFiles(List<File> files, Direction direction) {
 		boolean success = false;
 		List<SpeedMeasurement> entrysToAdd = new ArrayList<SpeedMeasurement>();
+		long startTime = System.currentTimeMillis();
 
 		System.out.println("Reading files for direction: " + direction.toString());
 		for (File file : files) {
@@ -30,17 +32,17 @@ public class DataImporter {
 
 				for (String line = in.readLine(); line != null; line = in.readLine()) {
 					String[] tokens = line.split(" ");
-					String date = parseDate(tokens[0]);
+					int date = parseDate(tokens[0]);
 					String time = tokens[1];
 					short speed_in = Short.parseShort(tokens[2]);
 					short speed_out = Short.parseShort(tokens[4]);
 
 					SpeedMeasurement sm = new SpeedMeasurement(date, time, direction, speed_in, speed_out);
 					// todo: optimize comparison
-					//if (!isInDb(sm)) {
-					entrysToAdd.add(sm);
-					speedMeasurementList.add(sm);
-					//}
+					if (!isInDb(sm)) {
+						entrysToAdd.add(sm);
+						speedMeasurements.get(date).add(sm);
+					}
 				}
 
 			} catch (IOException e) {
@@ -48,7 +50,7 @@ public class DataImporter {
 			}
 		}
 
-		System.out.println("	Found " + entrysToAdd.size() + " entries that need to be inserted");
+		System.out.println("	Found " + entrysToAdd.size() + " entries that need to be inserted, took " + (System.currentTimeMillis() - startTime) + "ms");
 
 		try {
 			sqlConnection.insertData(entrysToAdd);
@@ -60,6 +62,11 @@ public class DataImporter {
 	}
 
 	private boolean isInDb(SpeedMeasurement sm) {
+		if (!speedMeasurements.containsKey(sm.date)) {
+			speedMeasurements.put(sm.date, new ArrayList<SpeedMeasurement>());
+			return false;
+		}
+		List<SpeedMeasurement> speedMeasurementList = speedMeasurements.get(sm.date);
 		for (SpeedMeasurement speedMeasurementEntry : speedMeasurementList) {
 			if (speedMeasurementEntry.equals(sm)) {
 				return true;
@@ -68,16 +75,17 @@ public class DataImporter {
 		return false;
 	}
 
-	private String parseDate(String token) {
+	private int parseDate(String token) {
 		String[] dateTokens = token.split(Pattern.quote("."));
 		String day = dateTokens[0];
 		String month = dateTokens[1];
 		String year = "20" + dateTokens[2];
-		return year + "-" + month + "-" + day;
+		int date = Integer.parseInt(year + month + day);
+		return date;
 	}
 
 	private void loadDatabase() {
-		speedMeasurementList = sqlConnection.getAllEntrys();
+		speedMeasurements = sqlConnection.getAllEntrys();
 	}
 
 }
