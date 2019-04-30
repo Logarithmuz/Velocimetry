b0417f76c1c6cbbd5c2c7b3833672ac62724f2ad
Logarithmuz <40320189+Logarithmuz@users.noreply.github.com>
2019-04-30 17:58:17 +0200

    Serveradress and credentials are no longer hard coded

--- src/main/java/de/velocimetry/VelocimetryMain.java
+++ src/main/java/de/velocimetry/VelocimetryMain.java
@@ -1,15 +1,29 @@
 package de.velocimetry;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class VelocimetryMain {
 
 	public static void main(String[] args) {
-
+		String server = null, username = null, password = null;
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Please insert server adress: ");
+			server = in.readLine();
+			System.out.println("Please insert username: ");
+			username = in.readLine();
+			System.out.println("Please insert password: ");
+			password = in.readLine();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		SQLConnection sqlConnection = new SQLConnection();
-		sqlConnection.connect("localhost:3306", "velocimetry", "velocimetry_user", "po9KB76BVPh4p:^uOmEn");
+		sqlConnection.connect(server + ":3306", "data", username, password);
 
 		List<File> files_fest = getListOfFilesInFolder("speed_data/in(fest)");
 		List<File> files_mobil = getListOfFilesInFolder("speed_data/out(mobil)");
@@ -19,7 +33,7 @@ public class VelocimetryMain {
 		importer.importFromFiles(files_mobil, Device.MOBIL);
 	}
 
-	private static List<File> getListOfFilesInFolder(String path){
+	public static List<File> getListOfFilesInFolder(String path) {
 		File folder = new File(VelocimetryMain.class.getClassLoader().getResource(path).getPath());
 		return getListOfFilesInFolder(folder);
 	}
